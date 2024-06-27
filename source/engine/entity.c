0531d4b2151ad55bff336c2d7d359e68a79db7ae
Piotr Stradowski <stradowski.stradek@gmail.com>
2024-06-27 12:31:11 +0200

    Add files for entity component system implementation

---
+++ source/engine/entity.c
@@ -0,0 +1,10 @@
+#include "entity.h"
+
+struct 
+{
+    unsigned int id;
+    unsigned int componentMask;
+} Entity;
+
+
+
---
+++ source/engine/entity.h
@@ -0,0 +1,8 @@
+#ifndef ENTITY_H
+#define ENTITY_H
+
+struct Entity;  
+
+addEntity(Entity *entity);
+
+#endif // ENTITY_H

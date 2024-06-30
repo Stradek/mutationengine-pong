28c04d565a2a40fa83854b9bf11d37b051b34647
Piotr Stradowski <stradowski.stradek@gmail.com>
2024-06-30 22:29:57 +0200

    Add debug and release configuration support to build.bat

--- build.bat
+++ build.bat
@@ -1,11 +1,28 @@
 @echo off
-setlocal
+setlocal disabledelayedexpansion
 
-rem Set the project name
+rem Set the project name and build directory
 set PROJECT_NAME=MutationEngine-Pong
 set BUILD_DIR=build
-set BINARIES_DIR=bin
-set PROJECT_EXECUTABLE_PATH=%BINARIES_DIR%\%PROJECT_NAME%.exe
+
+rem set program configuration
+set arg1=%1 
+if "%1" == "Release" (
+    echo "Release configuration selected."
+    set BINARIES_DIR=bin\release
+    set PROJECT_EXECUTABLE_SUFFIX=
+) else if "%1" == "Debug" (
+    echo "Debug configuration selected."
+    set BINARIES_DIR=bin\debug
+    set PROJECT_EXECUTABLE_SUFFIX=-d
+) else (
+    set "ERROR_MESSAGE=Invalid configuration. Use 'Release' or 'Debug'."
+    call :handle_error
+    goto :EOF
+)
+
+set PROJECT_EXECUTABLE_PATH=%BINARIES_DIR%\%PROJECT_NAME%%PROJECT_EXECUTABLE_SUFFIX%.exe
+
 goto :main
 
 rem Function to handle error and exit
@@ -19,7 +36,7 @@ rem Configure the project
 if not exist %BUILD_DIR% ( 
     mkdir %BUILD_DIR%
 )
-cmake -G "MinGW Makefiles" -S . -B %BUILD_DIR%
+cmake -G "MinGW Makefiles" -S . -B %BUILD_DIR% -DCMAKE_BUILD_TYPE=%1 -DCMAKE_INSTALL_PREFIX=%BINARIES_DIR%
 if %errorlevel% neq 0 (
     set "ERROR_MESSAGE=Failed to configure the project."
     call :handle_error
@@ -42,6 +59,7 @@ if exist %PROJECT_EXECUTABLE_PATH% (
     call :handle_error
     goto :EOF
 )
+echo Program finished.
 pause
 
 endlocal
